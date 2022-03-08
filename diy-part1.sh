bb5d7feddd298cecb4809c2eab1c37513aeac5eb
benben720 <67833264+benben720@users.noreply.github.com>
2022-03-09 04:54:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,10 +16,9 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-#git clone https://github.com/jerrykuku/lua-maxminddb package/lean/lua-maxminddb
-#git clone https://github.com/jerrykuku/luci-app-vssr package/lean/luci-app-vssr
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon.git
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+git clone https://github.com/jerrykuku/lua-maxminddb package/lean/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr package/lean/luci-app-vssr
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome
